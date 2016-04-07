5fecd8c5443c73d48c5ef47886a6f2caf6d9d2fe
Daniël Franke <daniel@ams-sec.org>
2016-04-07 09:15:19 +0200

    Force locale in launcher.
    Also change font.

--- misc/.bin/launcher.sh
+++ misc/.bin/launcher.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+LANG=en_US.UTF-8
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/opt/android-sdk/tools:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/usr/local/sbin:/usr/local/bin:/usr/bin/vendor_perl/:/home/daniel/.bin:/home/daniel/.sbin"
 
-echo $PATH > /tmp/launcher
-font="Liberation Mono for Powerline-12"
+font="Roboto Mono-12"
 
-dmenu_run -i -h 1080 -w 1920 -q -o .9 -p " execute" -fn "$font" -nb "#000000" -nf "#bbbbbb" -sb "#000000" -sf "#850000"
+dmenu_run -i -h 1080 -w 1920 -q -o .9 -p " execute" -fn "$font" -nb "#000000" -nf "#bbbbbb" -sb "#000000" -sf "#850000" > /tmp/output 2>&1
